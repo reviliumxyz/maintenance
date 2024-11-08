7b8434df10bbb823c5ea79135f24f67192430f66
cirroskais <cirroskais@madhouselabs.net>
2024-11-08 05:57:16 -0500

    no relative paths

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { phosphorSvelteOptimize } from 'phosphor-svelte/preprocessor';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [phosphorSvelteOptimize(), vitePreprocess()],
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter(), paths: { relative: false } }
 };
 
 export default config;
